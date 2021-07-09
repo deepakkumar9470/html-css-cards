4e44f8fd288a5a9f2afd54b893ab1eebf67fd148
deepakkumar9470 <dk397787@gmail.com>
2021-07-09 13:24:17 +0530

    html-css-cards

---
+++ react-hooks/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Netliflix from './components/Netliflix';
+function App() {
+  return (
+       <>
+            <Netliflix name/>  
+           
+       </>
+  );
+}
+
+export default App;
---
+++ react-hooks/src/components/Netliflix.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Netliflix.css'
+const Netliflix = () => {
+    return (
+        <>
+        <h1>CSS Cards</h1>
+           <section className="container">
+             <div className="card">
+                        <div className="card_img card_1"></div>
+                       <h2>Deepak kumar</h2>
+                       <p>Hi I am deepak kumar chouhan.
+                       Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. unde.
+                       </p>
+                       <a href="#">Readmore</a>        
+             </div>
+             <div className="card">
+                        <div className="card_img card_2"></div>
+                       <h2>Deepak kumar</h2>
+                       <p>Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. unde.</p>
+                       <a href="#">Readmore</a>
+             </div>
+
+             <div className="card">
+                 
+                        <div className="card_img card_3"></div>
+                       <h2>Deepak kumar</h2>
+                       <p>Hi I am deepak kumar chouhan.
+                        Lorem ipsum dolor sit amet, 
+                        consectetur adipisicing elit. unde.
+                       </p>
+                       <a href="#">Readmore</a>
+             </div>
+           </section>
+            
+        </>
+    )
+}
+
+export default Netliflix
